1c5a19134f148e364f66b70f466aa7da4d224273
BT_1N3_18 <ctam9591@bths.edu>
2019-01-08 10:58:08 -0500

    update

--- src/sample/BackEnd.java
+++ src/sample/BackEnd.java
@@ -1,24 +1,77 @@
 package sample;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BackEnd
 {
-    Timer time;
+    private ArrayList<String> CSVData;
+    private ArrayList<String> names;
+    private ArrayList<Integer> scores;
+    private Path path;
+    private int numColumns;
 
-    public BackEnd(int num)
+    public BackEnd(String path)
     {
-        time = new Timer();
-        time.schedule(new RemindTask(), 0);
+        this.path = Paths.get(new File(path).getPath());
+        this.names = new ArrayList<>();
+        this.scores = new ArrayList<Integer>();
+        try (BufferedReader br = (BufferedReader) Files.newBufferedReader(Paths.get(path),
+                StandardCharsets.US_ASCII))
+        {
+            String line = br.readLine();
+            while (line != null)
+            {
+                String[] values = line.split(",");
+                names.add(values[0]);
+                scores.add(Integer.parseInt(values[1]));
+            }
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
     }
 
-    class RemindTask extends TimerTask
+    public List<String> getColumnHeaders()
     {
-        public void run()
-        {
-            System.out.println("Time is up!");
-            time.cancel();
+        return new ArrayList<>(Arrays.asList(this.CSVData.get(0).split(",")));
+    }
+
+    public List<String> getDataString(int column)
+    {
+        ArrayList<String> x = new ArrayList<String>();
+        for (int i = 1; i < this.CSVData.size(); i++) {
+            x.add(this.CSVData.get(i).split(",")[column]);
         }
+        return x;
+    }
+
+    public List<Integer> getDataInt(int column)
+    {
+        List<String> a = getDataString(column);
+        List<Integer> b = new ArrayList<>();
+        for (String str : a) {
+            System.out.println(str);
+            b.add(Integer.parseInt(str));
+        }
+        return b;
+    }
+
+    public List<Double> getDataDouble(int column)
+    {
+        List<String> a = getDataString(column);
+        List<Double> b = new ArrayList<>();
+        for (String str : a)
+            b.add(Double.parseDouble(str));
+        return b;
     }
 }
--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -1,28 +1,68 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import java.util.ArrayList;
+import javafx.util.Duration;
+
+import javax.xml.soap.Text;
 
 public class GameCode extends Application
 {
-    private ArrayList<Integer> sequence = new ArrayList<Integer>();
+    @FXML
+    private Button button1, button2;
+    private int score;
+    private int highscore = 0;
+    @FXML
+    private Text text;
+
+    public void click(ActionEvent actionEvent)
+    {
+        score++;
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(5000), ae -> endGame()));
+        timeline.play();
+    }
+
+    public void findScore(ActionEvent actionEvent)
+    {
+        button2.setText(Integer.toString(score));
+        //
+        // getHighScore();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Button Game");
-        primaryStage.setScene(new Scene(root, 200, 200));
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
+
+    public void endGame()
+    {
+        button1.setText("Game over!");
+        button1.setDisable(true);
+    }
+
+    /*public void getHighScore()
+    {
+        if (highscore == 0)
+            highscore = score;
+        else if (score > highscore)
+            highscore = score;
+        text.setTextContent(String.valueOf(highscore));
+    }*/
 
     public static void main(String[] args)
     {
         launch(args);
-        new BackEnd(1);
     }
 }
